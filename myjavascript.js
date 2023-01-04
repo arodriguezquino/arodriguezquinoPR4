e872386821838b1292e8bd0abea66f3c14c45401
arodriguezquino <121186120+arodriguezquino@users.noreply.github.com>
2023-01-04 20:40:17 +0100

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,15 +1,17 @@
-const provin = new Array("Alava","Albacete","Alicante","Almería","Asturias","Avila","Badajoz","Barcelona","Burgos","Cáceres",
-		"Cádiz","Cantabria","Castellón","Ciudad Real","Córdoba","La Coruña","Cuenca","Gerona","Granada","Guadalajara",
-		"Guipúzcoa","Huelva","Huesca","Islas Baleares","Jaén","León","Lérida","Lugo","Madrid","Málaga","Murcia","Navarra",
-		"Orense","Palencia","Las Palmas","Pontevedra","La Rioja","Salamanca","Segovia","Sevilla","Soria","Tarragona",
-		"Santa Cruz de Tenerife","Teruel","Toledo","Valencia","Valladolid","Vizcaya","Zamora","Zaragoza");
+window.onload = (event) => {
+	const provin = new Array("Alava","Albacete","Alicante","Almería","Asturias","Avila","Badajoz","Barcelona","Burgos","Cáceres",
+			"Cádiz","Cantabria","Castellón","Ciudad Real","Córdoba","La Coruña","Cuenca","Gerona","Granada","Guadalajara",
+			"Guipúzcoa","Huelva","Huesca","Islas Baleares","Jaén","León","Lérida","Lugo","Madrid","Málaga","Murcia","Navarra",
+			"Orense","Palencia","Las Palmas","Pontevedra","La Rioja","Salamanca","Segovia","Sevilla","Soria","Tarragona",
+			"Santa Cruz de Tenerife","Teruel","Toledo","Valencia","Valladolid","Vizcaya","Zamora","Zaragoza");
 
-var newSelect = document.getElementById("selesion");
+	var newSelect = document.getElementById("selesion");
 
-for(var i = 0, len = provin.length; i < len;++i)
-{
-  var opt = document.createElement("option");
-  opt.value= provin[i];
-  opt.text = provin[i];
-  newSelect.add(opt);
+	for(var i = 0, len = provin.length; i < len;++i)
+	{
+	  var opt = document.createElement("option");
+	  opt.value= provin[i];
+	  opt.text = provin[i];
+	  newSelect.add(opt);
+	}
 };
