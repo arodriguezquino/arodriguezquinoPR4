a997995344c028a8b505d89c785cb4749e41b42c
arodriguezquino <121186120+arodriguezquino@users.noreply.github.com>
2023-01-04 22:09:06 +0100

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,17 +1,15 @@
-$( document ).ready(function() {
-  const provin = new Array("Alava","Albacete","Alicante","Almería","Asturias","Avila","Badajoz","Barcelona","Burgos","Cáceres",
+const provin = new Array("Alava","Albacete","Alicante","Almería","Asturias","Avila","Badajoz","Barcelona","Burgos","Cáceres",
   "Cádiz","Cantabria","Castellón","Ciudad Real","Córdoba","La Coruña","Cuenca","Gerona","Granada","Guadalajara",
   "Guipúzcoa","Huelva","Huesca","Islas Baleares","Jaén","León","Lérida","Lugo","Madrid","Málaga","Murcia","Navarra",
   "Orense","Palencia","Las Palmas","Pontevedra","La Rioja","Salamanca","Segovia","Sevilla","Soria","Tarragona",
   "Santa Cruz de Tenerife","Teruel","Toledo","Valencia","Valladolid","Vizcaya","Zamora","Zaragoza");
 
-  const newSelect = document.querySelector("select");
-  console.log(newSelect);/*
-  for(var i = 0, len = provin.length; i < len;++i)
-  {
-  var opt = document.createElement("option");
-  opt.value= provin[i];
-  opt.text = provin[i];
-  newSelect.add(opt);
-  }*/
-});
+const newSelect = document.querySelector("select");
+console.log(newSelect);/*
+for(var i = 0, len = provin.length; i < len;++i)
+{
+var opt = document.createElement("option");
+opt.value= provin[i];
+opt.text = provin[i];
+newSelect.add(opt);
+}*/
