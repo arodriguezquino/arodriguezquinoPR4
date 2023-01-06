511d5d108bf4c6cdbe3f0039cb5e733533bcb8da
arodriguezquino <121186120+arodriguezquino@users.noreply.github.com>
2023-01-06 13:04:53 +0100

    Update myHabitats.js

--- myHabitats.js
+++ myHabitats.js
@@ -25,8 +25,8 @@ for(let i = 0; i < auxArray.length; i++) {
   var newSlide = document.createElement("img");
   
   newSlide.src = auxArray[i].fotos;
-  newSlide.height = "50px";
-  newSlide.width = "50px";
+  newSlide.height = "50";
+  newSlide.width = "50";
   
   newDiv.appendChild(newSlide);
   newDiv.appendChild(document.createElement("br"));
