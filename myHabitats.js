0a4283c99f9888583a58804b74929628ad115faa
arodriguezquino <121186120+arodriguezquino@users.noreply.github.com>
2023-01-06 12:40:36 +0100

    Update myHabitats.js

--- myHabitats.js
+++ myHabitats.js
@@ -1,5 +1,5 @@
 var tbody = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-const array = [{"titulo":"Piso mediano centro Vizcaya","localidad":"Vizcaya","m2":"108","hab":"2","bany":"1","fotos":"/img/hab1.jpg","precio":"1100€"},{"titulo":"Casa grande afueras Gerona","localidad":"Gerona","m2":"200","hab":"3","bany":"2","fotos":"/img/hab2.jpg","precio":"450000€"},{"titulo":"Estudio perfecto Barcelona rambla","localidad":"Barcelona","m2":"50","hab":"1","bany":"1","fotos":"/img/hab3.jpg","precio":"600€"},{"titulo":"Estudio magnifico centro Madrid sol","localidad":"Madrid","m2":"20","hab":"1","bany":"1","fotos":"/img/hab4.jpg","precio":"500€"},{"titulo":"Mansion Sevilla afueras","localidad":"Sevilla","m2":"1008","hab":"6","bany":"3","fotos":"/img/hab5.jpg","precio":"2500€"},{"titulo":"Casa Asturias centro","localidad":"Asturias","m2":"135","hab":"3","bany":"2","fotos":"/img/hab6.jpg","precio":"205999€"},{"titulo":"Masia Vizcaya afueras","localidad":"Vizcaya","m2":"1050","hab":"5","bany":"3","fotos":"/img/hab7.jpg","precio":"505999€"}];
+const array = [{"titulo":"Piso mediano centro Vizcaya","localidad":"Vizcaya","m2":"108","hab":"2","bany":"1","fotos":"img/hab1.jpg","precio":"1100€"},{"titulo":"Casa grande afueras Gerona","localidad":"Gerona","m2":"200","hab":"3","bany":"2","fotos":"img/hab2.jpg","precio":"450000€"},{"titulo":"Estudio perfecto Barcelona rambla","localidad":"Barcelona","m2":"50","hab":"1","bany":"1","fotos":"img/hab3.jpg","precio":"600€"},{"titulo":"Estudio magnifico centro Madrid sol","localidad":"Madrid","m2":"20","hab":"1","bany":"1","fotos":"img/hab4.jpg","precio":"500€"},{"titulo":"Mansion Sevilla afueras","localidad":"Sevilla","m2":"1008","hab":"6","bany":"3","fotos":"img/hab5.jpg","precio":"2500€"},{"titulo":"Casa Asturias centro","localidad":"Asturias","m2":"135","hab":"3","bany":"2","fotos":"img/hab6.jpg","precio":"205999€"},{"titulo":"Masia Vizcaya afueras","localidad":"Vizcaya","m2":"1050","hab":"5","bany":"3","fotos":"img/hab7.jpg","precio":"505999€"}];
 
 var url = new URL(window.location.href);
 var prov = url.searchParams.get("provincias")
@@ -25,6 +25,8 @@ for(let i = 0; i < auxArray.length; i++) {
   var newSlide = document.createElement("img");
   
   newSlide.src = auxArray[i].fotos;
+  newSlide.height = "5%";
+  newSlide.width = "5%";
   
   newDiv.appendChild(newSlide);
   newDiv.appendChild(document.createElement("br"));
